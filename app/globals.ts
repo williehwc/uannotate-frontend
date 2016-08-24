f7a60c181b16a1ab47df5a839a2ec20f753efa5d
Willie Chang <williehwc@gmail.com>
2016-08-24 14:33:37 -0400

    Changed backend location

--- app/globals.ts
+++ app/globals.ts
@@ -1,6 +1,6 @@
 import {Headers, RequestOptions} from '@angular/http';
 
-export var backendURL = 'https://uannotate-backend-williehwc.c9users.io';
+export var backendURL = 'http://bitnami-nodejs-193d.cloudapp.net';
 export var omimURL = 'http://omim.org/entry/';
 export var headers = new Headers({ 'Content-Type': 'application/json' });
 export var options = new RequestOptions({ headers: headers });
