8218c65b728ec0c1ad6396b0dee033835f7fe4a3
Willie Chang <williehwc@gmail.com>
2016-09-07 14:22:24 -0400

    Can't assign generated grade if no comparison

--- app/globals.ts
+++ app/globals.ts
@@ -1,6 +1,6 @@
 import {Headers, RequestOptions} from '@angular/http';
 
-export var backendURL = 'http://bitnami-nodejs-193d.cloudapp.net';
+export var backendURL = 'http://52.186.121.47:3000';
 export var omimURL = 'http://omim.org/entry/';
 export var headers = new Headers({ 'Content-Type': 'application/json' });
 export var options = new RequestOptions({ headers: headers });
